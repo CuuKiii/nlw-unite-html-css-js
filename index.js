aaf6dcd8cc91794808a70b639a58c7e273b86f0a
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2024-04-02 13:58:46 -0300

    Aula 1

---
+++ index.js
@@ -0,0 +1,100 @@
+let participantes = [
+  {
+    nome: "Leonardo Morais",
+    email: "leonardo@gmail.com",
+    dataInscricao: new Date(2024, 2, 1, 19, 23),
+    dataCheckIn: new Date(2024, 2, 1, 20, 20)
+  },
+  {
+    nome: "Luiza Aguiar",
+    email: "luluca@gmail.com",
+    dataInscricao: new Date(2024, 2, 23, 19, 23),
+    dataCheckIn: new Date(2024, 2, 25, 20, 20)
+  },
+  {
+    nome: "Yan Lemos",
+    email: "lemons@gmail.com",
+    dataInscricao: new Date(2024, 0, 3, 19, 23),
+    dataCheckIn: new Date(2024, 0, 4, 20, 20)
+  },
+  {
+    nome: "Enrico Alcantara",
+    email: "enrigay@gmail.com",
+    dataInscricao: new Date(2023, 11, 4, 19, 23),
+    dataCheckIn: new Date(2023, 11, 5, 20, 20)
+  },
+  {
+    nome: "Tulio Lamppz",
+    email: "lamppz@gmail.com",
+    dataInscricao: new Date(2023, 10, 5, 19, 23),
+    dataCheckIn: new Date(2023, 10, 6, 20, 20)
+  },
+  {
+    nome: "Pedro Santos",
+    email: "pedro@gmail.com",
+    dataInscricao: new Date(2023, 9, 6, 19, 23),
+    dataCheckIn: new Date(2023, 9, 7, 20, 20)
+  },
+  {
+    nome: "Italo Lima",
+    email: "italo@gmail.com",
+    dataInscricao: new Date(2023, 8, 7, 19, 23),
+    dataCheckIn: new Date(2023, 8, 8, 20, 20)
+  },
+  {
+    nome: "Tales Lima",
+    email: "tales@gmail.com",
+    dataInscricao: new Date(2023, 7, 8, 19, 23),
+    dataCheckIn: new Date(2023, 7, 9, 20, 20)
+  },
+  {
+    nome: "Pedro Sampaio",
+    email: "pedro@gmail.com",
+    dataInscricao: new Date(2023, 6, 9, 19, 23),
+    dataCheckIn: new Date(2023, 6, 10, 20, 20)
+  },
+  {
+    nome: "Gabriel Mendes",
+    email: "gabriel@gmail.com",
+    dataInscricao: new Date(2023, 5, 10, 19, 23),
+    dataCheckIn: new Date(2023, 5, 11, 20, 20)
+  }
+];
+
+const criarNovoParticipante = (participante) => {
+  const dataInscricao = dayjs(Date.now())
+  .to(participante.dataInscricao)
+
+  const dataCheckIn = dayjs(Date.now())
+  .to(participante.dataCheckIn)
+  
+  return `
+  <tr>
+    <td>
+      <strong>
+        ${participante.nome}
+      </strong>
+      <br>
+      <small>
+        ${participante.email}
+      </small>
+    </td>
+    <td>${dataInscricao}</td>
+    <td>${dataCheckIn}</td>
+  </tr>
+  `
+}
+
+const atualizarLista = (participantes) => {
+  let output = ""
+  for(let participante of participantes) {
+    output = output + criarNovoParticipante(participante)
+  }
+
+  // substituir informação do HTML
+  document
+  .querySelector('tbody')
+  .innerHTML = output
+}
+
+atualizarLista(participantes)
---
+++ index.md
@@ -0,0 +1,48 @@
+
+# HTML
+
+*Hypertext*
+
+*Markup*
+- Tag 
+- Atributos
+
+*Language*
+
+
+# CSS
+
+# JavaScript
+```js
+// variaveis
+const mensagem = `Oi, tudo bem?`
+// tipos de dados
+  // number
+  // string
+// funcao
+alert(mensagem)
+
+// objeto javascript
+const participante = {
+  nome: "Leonardo Morais",
+  email: "leonardo@gmail.com",
+  dataInscricao: new Date(2024, 2, 22, 19, 20),
+  dataCheckIn: new Date(2024, 2, 25, 22, 00)
+}
+
+// array
+let participantes = [
+  {
+    nome: "Leonardo Morais",
+    email: "leonardo@gmail.com",
+    dataInscricao: new Date(2024, 2, 22, 19, 20),
+    dataCheckIn: new Date(2024, 2, 25, 22, 00)
+  },
+]
+
+// estrutura de repetição - loop
+for(let participante of participantes) {
+  // faça alguma coisa aqui
+  // enquanto tiver participantes nessa lista
+}
+```
